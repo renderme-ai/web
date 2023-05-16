0eae7d0fcf913c9bdfef515eab309ce23e3e031d
Rick G <lacunainc04@gmail.com>
2023-05-16 16:22:02 -0700

    Simple image upload
    - Add react-bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/reset.css';
 import '../styles/main.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
--- pages/start/index.js
+++ pages/start/index.js
@@ -1,13 +1,31 @@
-const Start = () => (
-  <div className="home">
-    <h1>Upload your images</h1>
-    <input
-      id="fileUpload"
-      type="file"
-      multiple
-      accept="image/jpeg,image/png,image/gif"
-    />
-  </div>
-);
+import { useState } from 'react';
+
+const Start = () => {
+  const [files, setFiles] = useState([]);
+  return (
+    <div className="home">
+      <h1>Upload your images</h1>
+      <input
+        id="fileUpload"
+        type="file"
+        multiple
+        accept="image/jpeg,image/png,image/gif"
+        onChange={(event) => {
+          let images = [];
+          for (let i = 0; i < event.target.files.length; i++) {
+            images.push(URL.createObjectURL(event.target.files[i]));
+          }
+          setFiles(images);
+        }}
+      />
+      <div>
+        {files.length > 0 &&
+          files.map((file, index) => (
+            <img key={`${file}-${index}`} alt={`uploaded ${index + 1}`} src={file} />
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default Start;
