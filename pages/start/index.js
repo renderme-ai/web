a95fcd525da486928d116643db930d775fc02427
Rick G <lacunainc04@gmail.com>
2023-05-16 20:19:43 -0700

    Quick test for first funky request

--- pages/start/index.js
+++ pages/start/index.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import ImagesControl from '../../components/imagesControl';
 import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 const Start = () => {
   const [files, setFiles] = useState([]);
@@ -26,7 +27,22 @@ const Start = () => {
         <Row>{files.length > 0 && <ImagesControl files={files} />}</Row>
         <Row style={{ padding: '10px' }}>
           {files.length > 0 && (
-            <Button variant="primary" size="lg">
+            <Button
+              variant="primary"
+              size="lg"
+              onClick={() => {
+                axios
+                  .get(
+                    'https://rendermefx.azurewebsites.net/api/MainHttpTrigger'
+                  )
+                  .then((res) => {
+                    console.log('success', res);
+                  })
+                  .catch((err) => {
+                    console.log('error!!!', err);
+                  });
+              }}
+            >
               Start Training
             </Button>
           )}
